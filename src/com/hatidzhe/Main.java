0cb513c7e0205960cafd364ef5b1d611a09922b5
Hatidzhe <yusufova.h@gmail.com>
2015-08-30 11:11:51 +0300

    chg:new version1

---
+++ src/com/hatidzhe/Main.java
@@ -0,0 +1,10 @@
+package com.hatidzhe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] primes = {1, 2, 3, 4, 5};
+        for (int i = 0; i < primes.length; i++)
+            System.out.println(primes[i]);
+    }
+}
